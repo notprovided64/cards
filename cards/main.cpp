bd51c785919786d90df61ddb955c002bd526db7e
Greg Clayton <gclayton@fb.com>
2020-03-21 20:51:49 -0700

    Fixup things to be in classes, add unions for easy number and enum access.

--- cards/main.cpp
+++ cards/main.cpp
@@ -3,130 +3,141 @@
 #include <cstdlib>
 #include <random>
 
-enum NUMBER {one, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king};
-enum SUIT {club, diamond, heart, spade};
+enum NUMBER : uint8_t {invalid_number, ace, two, three, four, five, six, seven,
+                       eight, nine, ten, jack, queen, king};
+enum SUIT : uint8_t {invalid_suit, club, diamond, heart, spade};
 
-struct Card
-{
-     int number;
-     int suit;
+#define ENUM_TO_CSTR(e) case e: return #e; break
+
+struct Card {
+  
+  union {
+    uint8_t i;
+    NUMBER e;
+  } number;
+  union {
+    uint8_t i;
+    SUIT e;
+  } suit;
+  
+  Card(int n, int s) {
+    number.i = n;
+    suit.i = s;
+  }
+
+  Card(NUMBER n, SUIT s) {
+    number.e = n;
+    suit.e = s;
+  }
+  
+  bool isValid() const {
+    return ace <= number.i && number.i <= king && club <= suit.i && suit.i <= spade;
+  }
+  
+  operator bool() const {
+    return isValid();
+  }
+
+  std::string number_str() const {
+      switch (number.e)
+      {
+          ENUM_TO_CSTR(ace);
+          ENUM_TO_CSTR(two);
+          ENUM_TO_CSTR(three);
+          ENUM_TO_CSTR(four);
+          ENUM_TO_CSTR(five);
+          ENUM_TO_CSTR(six);
+          ENUM_TO_CSTR(seven);
+          ENUM_TO_CSTR(eight);
+          ENUM_TO_CSTR(nine);
+          ENUM_TO_CSTR(ten);
+          ENUM_TO_CSTR(jack);
+          ENUM_TO_CSTR(queen);
+          ENUM_TO_CSTR(king);
+        default:
+          return "<invalid card number>";
+          break;
+      }
+  }
+  std::string suit_str() const
+  {
+      switch (suit.e)
+      {
+          ENUM_TO_CSTR(club);
+          ENUM_TO_CSTR(diamond);
+          ENUM_TO_CSTR(heart);
+          ENUM_TO_CSTR(spade);
+          default:
+            return "<invalid card suit>";
+            break;
+      }
+  }
+
+  void dump() const {
+    printf("Your card is a %s of %s\n",
+           number_str().c_str(), suit_str().c_str());
+  }
 };
 
-struct Cards
+class Cards
 {
-	std::vector<Card> order;
+private:
+  std::vector<Card> m_cards;
+
+public:
+  static Cards makeStandardDeck(bool shuffle)
+  {
+      Cards deck;
+      for (int suit = club; suit != spade+1; suit++)
+        for (int number = ace; number != king+1; number++)
+              deck.append(Card(number, suit));
+      if (shuffle)
+          deck.shuffle();
+      return deck;
+  }
+
+  void shuffle()
+  {
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::shuffle(m_cards.begin(), m_cards.end(), gen);
+  }
+
+  void append(Card card) {
+    m_cards.push_back(card);
+  }
+
+  const Card &operator[](size_t i) {
+    return m_cards[i];
+  }
+  
+  void dump() const {
+    for (Card card: m_cards)
+      card.dump();
+  }
+  
+  Card getTopCard() {
+    assert(!m_cards.empty());
+    Card card = m_cards.back();
+    m_cards.pop_back();
+    return card;
+  }
+  
+  Cards getCards(size_t count)
+  {
+    Cards cards;
+    for (size_t i=0; i<count; ++i)
+      cards.append(getTopCard());
+    return cards;
+  }
 };
 
-std::string NUMBER_string(int card)
-{
-    switch (card)
-    {
-        case 0:
-            return "one";
-        case 1:
-            return "two";
-        case 2:
-            return "three";
-        case 3:
-            return "four";
-        case 4:
-            return "five";
-        case 5:
-            return "six";
-        case 6:
-            return "seven";
-        case 7:
-            return "eight";
-        case 8:
-            return "nine";
-        case 9:
-            return "ten";
-        case 10:
-            return "jack";
-        case 11:
-            return "queen";
-        case 12:
-            return "king";
-        default:
-            return "error";
-            break;
-    }
-}
-std::string SUIT_string(int card)
-{
-    switch (card)
-    {
-        case 0:
-            return "club";
-        case 1:
-            return "diamond";
-        case 2:
-            return "heart";
-        case 3:
-            return "spade";
-        default:
-            return "error";
-            break;
-    }
-}
-
-void printCards(Cards cards)
-{
-    for(int i=0; i<cards.order.size(); ++i)
-    {
-        printf("Your card is a %s of %s \n",NUMBER_string(cards.order[i].number).c_str(), SUIT_string(cards.order[i].suit).c_str());
-    }
-}
-
-Cards makeStdDeck()
-{
-    Cards deck;
-	for (int i = one; i != king+1; i++)
-	{
-   		for (int j = club; j != spade+1; j++)
-   		{
-			Card mid;
-			mid.number=i;
-			mid.suit=j;
-			deck.order.push_back(mid);
-   		}    
-	}    return deck;
-}
-
-Cards shuffleCards(Cards cards)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    std::shuffle(cards.order.begin(), cards.order.end(), gen);
-    return cards;
-}
-
-Cards dealHand(Cards cards)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(1,6);
-    
-    return cards;
-}
 
 int main(int argc, const char * argv[])
 {
-    Card ThreeOfSpades;
-    ThreeOfSpades.number=three;
-    ThreeOfSpades.suit=spade;
-	
-	Cards deck = makeStdDeck();
-    
-    printCards(deck);
-	
-    printf("\nnow to shuffle \n\n");
-    
-    deck = shuffleCards(deck);
-    
-    printCards(deck);
-    
+    Cards deck = Cards::makeStandardDeck(true);
+    Cards six = deck.getCards(6);
+    six.dump();
+
     return 0;
 }
